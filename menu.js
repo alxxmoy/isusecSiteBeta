d677b481ec2b1629a56656cb5a6b88c38a2840ff
ifiguer <isaac.229@hotmail.com>
2017-11-15 20:22:04 -0600

    Remove redundant code

--- menu.js
+++
@@ -1,6 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").classList.toggle("ghost"); 
-
-    let menuButton = document.getElementById("menu-button");
-    menuButton.innerHTML = menuButton.innerHTML == "☰" ? "×" : "☰";
-}
